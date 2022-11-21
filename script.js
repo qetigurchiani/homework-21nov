3e2cee8c4d962bc6a5cacd1a773f076803b991ba
qetigurchiani <114924207+qetigurchiani@users.noreply.github.com>
2022-11-21 21:28:29 +0400

    added style details

--- script.js
+++ script.js
@@ -1,32 +1,84 @@
-const userswrapper = document.querySelector(".users");
-const usrlist = document.querySelector(".usersList");
+const usersWrapper = document.querySelector(".Users");
+const usList = document.querySelector(".usersList");
 
-function getUsers(){
-    fetch("https://reqres.in/api/unknown", {method:"GET"},)
-    .then(res=>{
-       if(res.status !== 200){
+function getUsers1() {
+  fetch("https://reqres.in/api/unknown", { method: "GET" })
+    .then((res) => {
+      if (res.status !== 200) {
         throw new Error();
-       }
-       return res.json();
+      }
+      return res.json();
     })
-    .then(res=>{
-        const fragment = document.createDocumentFragment();
-        res.data.forEach(x=>{
-            const li = document.createElement('li');
-            li.innerText = `${x.name} Born in:  ${x.year}`;
-            fragment.appendChild(li);
-        });
+    .then((res) => {
+      const fragment = document.createDocumentFragment();
+      res.data.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerText = `${item.name} ${item.year}`;
+        fragment.appendChild(li);
+      });
 
-        usrlist.appendChild(fragment);
+      usList.appendChild(fragment);
     })
-    .catch(err=>{
-        const p = document.createElement('p');
-        p.innerText = err.message;
-        users-wrapper.appendChild(p);
-    })
+    .catch((err) => {
+      const p = document.createElement("p");
+      p.innerText = err.message;
+      usersWrapper.appendChild(p);
+    });
 }
 
-getUsers();
+getUsers1();
 
+// fetch-> xml http requist
+const usersWrapper1 = document.querySelector(".users");
+const list = document.querySelector(".userList");
+const next = document.getElementById("forward");
+const previous = document.getElementById("backward");
 
+let totalPages;
+let current = 1;
 
+function getUsers(pageNum) {
+  let request = new XMLHttpRequest();
+  request.addEventListener("load", () => {
+    let responsetxt = request.responseText;
+    let parsedjs = JSON.parse(responsetxt);
+    const fragment = new DocumentFragment();
+    parsedjs.data.forEach((user) => {
+      let li = document.createElement("li");
+      li.innerText = `${user.first_name} ${user.last_name}`;
+      fragment.appendChild(li);
+    });
+    list.innerHTML = " ";
+    list.appendChild(fragment);
+    totalPages = parsedjs.total_pages;
+  });
+
+  request.addEventListener("error", () => {
+    let p = document.createElement("p");
+    p.innerText = "404 Server Not Found";
+    usersWrapper.appendChild(p);
+  });
+
+  request.open("get", "https://reqres.in/api/users?page=" + pageNum);
+  request.send();
+}
+
+getUsers(current);
+
+previous.addEventListener("click", () => {
+  if (current === 1) {
+    return;
+  }
+
+  current--;
+  getUsers(current);
+});
+
+next.addEventListener("click", () => {
+  if (current === totalPages) {
+    return;
+  }
+
+  current++;
+  getUsers(current);
+});
